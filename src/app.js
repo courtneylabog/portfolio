1e3aaa8e1d0087da4eb3734b93ff5cd7597d50bd
Courtney Labog <labogcourtney@gmail.com>
2017-03-27 16:08:25 -0400

    Updated media queries and CSS transforms

--- src/app.js
+++ src/app.js
@@ -5,14 +5,29 @@ app.init = function (){
 	app.events();
 };
 
-
 app.events = function(){
 $('#FED').hover(
 	function(){
 	console.log('thisworks')
 	$('#svgSpan').toggleClass('hidden');
-	// $('#svgSpan').addClass('hidden');
+	$('#svgSpan').fadeToggle(2000);
+	// $('#svgSpan').fadeIn(600);
+	// $('#svgSpan').fadeOut(600);
 	})
+	$(function(){console.log('yo, this is a test');
+	    $('a[href*="#"]:not([href="#"])').click(function() {
+	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+	      var target = $(this.hash);
+	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	      if (target.length) {
+	        $('html, body').animate({
+	          scrollTop: target.offset().top
+	        }, 1000);
+	        return false;
+	      }
+	    }
+	  });
+	 }); 
 }
 
 // doc ready:
